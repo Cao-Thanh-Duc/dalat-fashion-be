4eb6c1ff53fb3d7224e39fa6a7780d1143a7d4ac
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2024-12-07 22:01:55 +0700

    feat: add api product, supplier, product-category

--- src/modules/category-product/category-product.module.ts
+++ src/modules/category-product/category-product.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
 import { CategoryProductController } from './category-product.controller';
 import { CategoryProductService } from './category-product.service';
 
 @Module({
   controllers: [CategoryProductController],
-  providers: [CategoryProductService]
+  providers: [CategoryProductService, PrismaService, JwtService],
 })
 export class CategoryProductModule {}
